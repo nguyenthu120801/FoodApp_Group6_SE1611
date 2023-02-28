e917810cdf47697dcf8ee8b7aefa3c1283d20243
Nguyễn Thị Thu <74730786+nguyenthu120801@users.noreply.github.com>
2023-03-01 02:25:49 +0700

    introActivity

---
+++ app/src/main/java/com/example/foodapp/IntroActivity.java
@@ -0,0 +1,26 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class IntroActivity extends AppCompatActivity {
+    private ConstraintLayout btn_start;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        btn_start = findViewById(R.id.btn_start);
+        btn_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+            }
+        });
+    }
+}
