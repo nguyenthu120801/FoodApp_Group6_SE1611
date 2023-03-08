219039216cec699904a02c634963e30f7110278b
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-03-08 11:04:53 +0700

    continue AddtoCart

--- app/src/main/java/com/example/foodapp/IntroActivity.java
+++ app/src/main/java/com/example/foodapp/IntroActivity.java
@@ -19,7 +19,7 @@ public class IntroActivity extends AppCompatActivity {
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IntroActivity.this, MainActivity.class));
+                startActivity(new Intent(IntroActivity.this, AddToCartActivity.class));
             }
         });
     }
