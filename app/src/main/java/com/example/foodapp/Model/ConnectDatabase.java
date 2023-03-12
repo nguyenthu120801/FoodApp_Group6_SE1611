5be1cd2aa9c98ea31f6cca8b174ef3a77984e9c8
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-03-12 21:42:00 +0700

    Update ConnectDatabase.java

--- app/src/main/java/com/example/foodapp/Model/ConnectDatabase.java
+++ app/src/main/java/com/example/foodapp/Model/ConnectDatabase.java
@@ -8,7 +8,6 @@ import androidx.annotation.Nullable;
 
 public class ConnectDatabase extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "APP_FOOD_PRM392";
-
     private static final int DATABASE_VERSION = 1;
     public ConnectDatabase(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
