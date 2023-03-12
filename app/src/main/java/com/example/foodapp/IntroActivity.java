949099e09c96a8833ecd4adaa3da883a76922699
Nguyễn Thị Thu <74730786+nguyenthu120801@users.noreply.github.com>
2023-03-12 23:47:40 +0700

    Thu update login

--- app/src/main/java/com/example/foodapp/IntroActivity.java
+++ app/src/main/java/com/example/foodapp/IntroActivity.java
@@ -21,7 +21,7 @@ public class IntroActivity extends AppCompatActivity {
         btn_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(IntroActivity.this, FoodDetailActivity.class));
+                startActivity(new Intent(IntroActivity.this, MainActivity.class));
             }
         });
     }
