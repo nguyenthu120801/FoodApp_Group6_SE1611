7ad1239f21b1b3b9cc1c8d421cd5c133ef50663f
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-03-13 00:03:10 +0700

    quân

--- app/src/main/java/com/example/foodapp/AddToCartActivity.java
+++ app/src/main/java/com/example/foodapp/AddToCartActivity.java
@@ -12,9 +12,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.foodapp.Entity.Cart;
 import com.example.foodapp.Entity.Food;
 import com.example.foodapp.Entity.OrderDetail;
 import com.example.foodapp.Entity.Product;
+import com.example.foodapp.Model.DAOCart;
 import com.example.foodapp.Model.DAOOrderDetail;
 import com.example.foodapp.Model.DAOProduct;
 import com.example.foodapp.activity.OrderActivity;
@@ -27,7 +29,7 @@ public class AddToCartActivity extends AppCompatActivity implements onChangeItem
     TextView tv_total;
     TextView tv_notification;
     List<Product> productList = new ArrayList<>();
-    List<OrderDetail> orderDetailList = new ArrayList<>();
+    List<Cart> cartList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,25 +37,25 @@ public class AddToCartActivity extends AppCompatActivity implements onChangeItem
 
         tv_total = findViewById(R.id.tv_totalPrice);
         tv_notification = findViewById(R.id.tv_noti);
-        orderDetailList = new DAOOrderDetail(this).getListOrderDetail(2);
+        cartList = new DAOCart(this).getListCart(2);
         rcv = findViewById(R.id.rv_category);
         double total = 0;
         int pID = 0;
         int id = getIntent().getIntExtra("id", 0);
         Product product = new DAOProduct(this).getProduct(id);
-        if(product != null || orderDetailList.size() != 0) {
-            for (OrderDetail o : orderDetailList) {
-               if(o.getProductID() == id){
+        if(product != null || cartList.size() != 0) {
+            for (Cart c : cartList) {
+               if(c.getProductID() == id){
                    pID = id;
-                   o.setQuantity(o.getQuantity() + 1);
-                   int n = new DAOOrderDetail(this).UpdateOrderDetail(new OrderDetail(o.getDetailID(), o.getOrderID(), o.getProductID(), o.getQuantity()));
+                   c.setQuantity(c.getQuantity() + 1);
+                   int n = new DAOCart(this).UpdateCart(c);
                }
-               productList.add(new DAOProduct(this).getProduct(o.getProductID()));
+               productList.add(new DAOProduct(this).getProduct(c.getProductID()));
 
             }
             if(pID != id){
-                orderDetailList.add(new OrderDetail(2, id, 1));
-                new DAOOrderDetail(this).AddOrderDetail(new OrderDetail(2, id, 1));
+                cartList.add(new Cart(2, id, 1));
+                new DAOCart(this).AddCart(new Cart(2, id, 1));
                 productList.add(new DAOProduct(this).getProduct(id));
             }
 
@@ -63,7 +65,7 @@ public class AddToCartActivity extends AppCompatActivity implements onChangeItem
             for (Product p : productList) {
                 total += p.getPrice();
             }
-            FoodAdapter adapter = new FoodAdapter(orderDetailList, total, productList, this::onPriceChange);
+            FoodAdapter adapter = new FoodAdapter(cartList, total, productList, this::onPriceChange);
             rcv.setLayoutManager(new LinearLayoutManager(this));
             rcv.setAdapter(adapter);
         }else{
--- app/src/main/java/com/example/foodapp/Model/DAOCart.java
+++ app/src/main/java/com/example/foodapp/Model/DAOCart.java
@@ -1,2 +1,65 @@
-package com.example.foodapp.Model;public class DAOCart {
+package com.example.foodapp.Model;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import androidx.annotation.Nullable;
+
+import com.example.foodapp.Entity.Cart;
+import com.example.foodapp.Entity.OrderDetail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOCart extends ConnectDatabase{
+
+    public DAOCart(@Nullable Context context) {
+        super(context);
+    }
+
+    public List<Cart> getListCart(int UserID){
+        List<Cart> list = new ArrayList<>();
+        SQLiteDatabase lite = getReadableDatabase();
+        String sql = "Select * from Cart where UserID = ?";
+        String[] selectionArgs = {String.valueOf(UserID)};
+        Cursor cursor = lite.rawQuery(sql , selectionArgs);
+        while(cursor!= null && cursor.moveToNext()){
+            int cartID = cursor.getInt(0);
+            int userID = cursor.getInt(1);
+            int productId = cursor.getInt(2);
+            int quantity = cursor.getInt(3);
+            Cart cart = new Cart(cartID, userID, productId, quantity);
+            list.add(cart);
+        }
+        return list;
+    }
+
+    public long AddCart (Cart cart){
+        SQLiteDatabase lite = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("UserID",cart.getUserID());
+        values.put("ProductID", cart.getProductID());
+        values.put("quantity",cart.getQuantity());
+        return lite.insert("Cart",null,values);
+    }
+
+    public int UpdateCart (Cart cart){
+        SQLiteDatabase lite = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        String whereClause = "CartID = ?";
+        String[] whereArgs = {String.valueOf(cart.getCartID())};
+        values.put("UserID",cart.getUserID());
+        values.put("ProductID", cart.getProductID());
+        values.put("quantity",cart.getQuantity());
+        return lite.update("Cart",values,whereClause,whereArgs);
+    }
+
+    public int DeleteCart (int cartID){
+        SQLiteDatabase lite = getWritableDatabase();
+        String whereClause = "CartID = ?";
+        String[] whereArgs = {String.valueOf(cartID)};
+        return lite.delete("Cart",whereClause,whereArgs);
+    }
 }
