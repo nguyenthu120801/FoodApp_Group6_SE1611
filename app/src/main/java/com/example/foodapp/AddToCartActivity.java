c9313e36d06fd96c683b5b18e8d08a66359db0d9
chuquan22 <81926487+chuquan22@users.noreply.github.com>
2023-03-13 22:13:22 +0700

    Cart

--- app/src/main/java/com/example/foodapp/AddToCartActivity.java
+++ app/src/main/java/com/example/foodapp/AddToCartActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.foodapp.Entity.Cart;
@@ -29,7 +30,7 @@ public class AddToCartActivity extends AppCompatActivity implements onChangeItem
     TextView tv_total;
     TextView tv_notification;
     List<Product> productList = new ArrayList<>();
-    List<Cart> cartList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,47 +38,10 @@ public class AddToCartActivity extends AppCompatActivity implements onChangeItem
 
         tv_total = findViewById(R.id.tv_totalPrice);
         tv_notification = findViewById(R.id.tv_noti);
-        cartList = new DAOCart(this).getListCart(2);
+        List<Cart> cartList = new DAOCart(this).getListCart(2);
         rcv = findViewById(R.id.rv_category);
-        double total = 0;
-        int pID = 0;
         int id = getIntent().getIntExtra("id", 0);
-        Product product = new DAOProduct(this).getProduct(id);
-        if(product != null || cartList.size() != 0) {
-            for (Cart c : cartList) {
-               if(c.getProductID() == id){
-                   pID = id;
-                   c.setQuantity(c.getQuantity() + 1);
-                   int n = new DAOCart(this).UpdateCart(c);
-               }
-               productList.add(new DAOProduct(this).getProduct(c.getProductID()));
-
-            }
-            if(pID != id){
-                cartList.add(new Cart(2, id, 1));
-                new DAOCart(this).AddCart(new Cart(2, id, 1));
-                productList.add(new DAOProduct(this).getProduct(id));
-            }
-
-
-        }
-        if(productList.size() != 0) {
-            for (Product p : productList) {
-                total += p.getPrice();
-            }
-            FoodAdapter adapter = new FoodAdapter(cartList, total, productList, this::onPriceChange);
-            rcv.setLayoutManager(new LinearLayoutManager(this));
-            rcv.setAdapter(adapter);
-        }else{
-            tv_notification.setText("Cart is empty, please buy food to continues");
-            tv_notification.setVisibility(View.VISIBLE);
-            tv_notification.setTextColor(Color.RED);
-            ((Button)findViewById(R.id.btn_checkout)).setVisibility(View.GONE);
-            tv_total.setVisibility(View.GONE);
-            ((TextView)findViewById(R.id.tv_text)).setVisibility(View.GONE);
-        }
-        
-
+        LoadRecyclerView(cartList, id);
 
         ((Button)findViewById(R.id.btn_checkout)).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -85,11 +49,56 @@ public class AddToCartActivity extends AppCompatActivity implements onChangeItem
                 Intent intent = new Intent(AddToCartActivity.this, OrderActivity.class);
             }
         });
-
     }
 
     @Override
     public void onPriceChange(double price) {
         tv_total.setText("$" + price);
+    }
+
+    @Override
+    public void onDeleteItem(int cartID) {
+        Log.d("size", String.valueOf(cartID));
+        new DAOCart(this).DeleteCart(cartID);
+        List<Cart> cartList = new DAOCart(this).getListCart(2);
+        LoadRecyclerView(cartList, -1);
+    }
+
+    public void LoadRecyclerView(List<Cart> cartList, int id){
+        double total = 0;
+        int pID = 0;
+
+        Product product = new DAOProduct(this).getProduct(id);
+        if(product != null) {
+            for (Cart c : cartList) {
+                if(c.getProductID() == id){
+                    pID = id;
+                    c.setQuantity(c.getQuantity() + 1);
+                    int n = new DAOCart(this).UpdateCart(c);
+                }
+            }
+            if(pID != id){
+                cartList.add(new Cart(2, product.getProductID(), 1));
+                new DAOCart(this).AddCart(new Cart(2, product.getProductID(), 1));
+            }
+        }
+        if(cartList.size() != 0) {
+            for (Cart c : cartList){
+                Product p = new DAOProduct(this).getProduct(c.getProductID());
+                total += p.getPrice() * c.getQuantity();
+                productList.add(p);
+            }
+            FoodAdapter adapter = new FoodAdapter(cartList, total, productList, this);
+            rcv.setLayoutManager(new LinearLayoutManager(this));
+            rcv.setAdapter(adapter);
+        }else{
+
+            tv_notification.setText("Cart is empty, please buy food to continues");
+            tv_notification.setVisibility(View.VISIBLE);
+            tv_notification.setTextColor(Color.RED);
+            ((Button)findViewById(R.id.btn_checkout)).setVisibility(View.GONE);
+            tv_total.setVisibility(View.GONE);
+            ((TextView)findViewById(R.id.textView10)).setVisibility(View.GONE);
+        }
     }
 }
