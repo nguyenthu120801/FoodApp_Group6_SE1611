c8123dc8f1744cffa74b41f3bfa1cfc742037ce0
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-03-14 18:41:32 +0700

    update bai lam

---
+++ app/src/main/java/com/example/foodapp/Adapter/ViewProductAdapter.java
@@ -0,0 +1,56 @@
+package com.example.foodapp.Adapter;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.foodapp.Entity.Product;
+import com.example.foodapp.R;
+
+import java.util.List;
+
+public class ViewProductAdapter extends BaseAdapter {
+    private List<Product> list;
+
+    public ViewProductAdapter(List<Product> list) {
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return list.get(i).getProductID();
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(view == null){
+            view =  View.inflate(viewGroup.getContext(), R.layout.item_product, null);
+        }
+        Product product = (Product) getItem(i);
+        TextView textName = view.findViewById(R.id.text_product_name);
+        TextView textPrice = view.findViewById(R.id.text_price);
+        ImageView image = view.findViewById(R.id.image);
+        textName.setText(product.getProductName());
+        textPrice.setText(product.getPrice() + "$");
+        image.setImageResource(product.getImage());
+        Drawable drawable = image.getDrawable();
+        Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap() ;
+        image.setImageBitmap(bitmap);
+        return view;
+    }
+}
