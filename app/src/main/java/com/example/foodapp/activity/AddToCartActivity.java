dc12b883cd9080e4fc6b952de28214e1cf7675b3
Nguyễn Thị Thu <74730786+nguyenthu120801@users.noreply.github.com>
2023-03-15 12:06:35 +0700

    Thu

--- app/src/main/java/com/example/foodapp/activity/AddToCartActivity.java
+++ app/src/main/java/com/example/foodapp/activity/AddToCartActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,16 +39,18 @@ import java.util.List;
 public class AddToCartActivity extends AppCompatActivity implements onChangeItem {
     RecyclerView rcv;
     TextView tv_total;
+    LinearLayout homebtn;
     ItemTouchHelper.SimpleCallback simpleCallback;
     List<Cart> cartList = new ArrayList<>();
     OrderDBHelper orderDBHelper;
     DAOOrderDetail daoOrderDetail;
     EditText addressText;
-DAOUser daoUser;
-Button checkoutBtn;
+    DAOUser daoUser;
+    Button checkoutBtn;
     SessionManager sessionManager;
     FoodAdapter adapter;
     int userID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -64,9 +67,15 @@ Button checkoutBtn;
         rcv = findViewById(R.id.rv_category);
         int id = getIntent().getIntExtra("id", 0);
         LoadRecyclerView(cartList, id);
+        homebtn = findViewById(R.id.homeBtn);
+        homebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddToCartActivity.this, MainActivity.class));
+            }
+        });
 
-
-         simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
+        simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT | ItemTouchHelper.LEFT) {
             @Override
             public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
                 return false;
@@ -76,16 +85,16 @@ Button checkoutBtn;
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 rcv.getAdapter();
                 int n = new DAOCart(AddToCartActivity.this).DeleteCart(cartList.get(viewHolder.getAdapterPosition()).getCartID());
-                if(n>0){
+                if (n > 0) {
                     Toast.makeText(AddToCartActivity.this, "Remove successfully", Toast.LENGTH_LONG).show();
 
-                }else{
+                } else {
                     Toast.makeText(AddToCartActivity.this, "Remove Fail", Toast.LENGTH_LONG).show();
                 }
                 cartList.remove(viewHolder.getAdapterPosition());
                 adapter.notifyDataSetChanged();
                 adapter.notifyItemRemoved(viewHolder.getAdapterPosition());
-                LoadRecyclerView( new DAOCart(AddToCartActivity.this).getListCart(userID), -1);
+                LoadRecyclerView(new DAOCart(AddToCartActivity.this).getListCart(userID), -1);
 
             }
         };
@@ -98,14 +107,14 @@ Button checkoutBtn;
                 Intent intent = new Intent(AddToCartActivity.this, OrderActivity.class);
             }
         });
-       orderDBHelper = new OrderDBHelper(this);
-       daoOrderDetail = new DAOOrderDetail(this);
+        orderDBHelper = new OrderDBHelper(this);
+        daoOrderDetail = new DAOOrderDetail(this);
         checkoutBtn = findViewById(R.id.btn_checkout);
         checkoutBtn.setOnClickListener(view -> checkout());
 
     }
 
-    private void checkout(){
+    private void checkout() {
         if (addressText.getText().toString().trim().isEmpty()) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle("Thông báo");
@@ -117,34 +126,34 @@ Button checkoutBtn;
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Order order = new Order();
         order.setUserID(userID);
-        order.setOrderDate( dateFormat.format(new Date()));
+        order.setOrderDate(dateFormat.format(new Date()));
         order.setStatus(Order.STATUS_IN_PROGRESS);
         order.setAddress(addressText.getText().toString());
-       int orderid = orderDBHelper.insertOrder(order);
-       Log.d("infoOrder", "order id vừa insert là : "+orderid);
-       if (orderid != -1){
-           Log.d("infoOrder","insert order successful" );
-           cartList = new DAOCart(this).getListCart(sessionManager.getUserID());
-           Log.d("infoOrder", "Số lượng cart là : " + cartList.size());
-           OrderDetail orderDetail = new OrderDetail();
-           for (Cart cart : cartList){
-               Log.d("infoOrder", "Cart : "+ cart);
-               orderDetail.setOrderID(orderid);
-               orderDetail.setProductID(cart.getProductID());
-               orderDetail.setQuantity(cart.getQuantity());
-               Log.d("infoOrder", "Order Detail add vào db : " + orderDetail);
-               daoOrderDetail.AddOrderDetail(orderDetail);
-           }
+        int orderid = orderDBHelper.insertOrder(order);
+        Log.d("infoOrder", "order id vừa insert là : " + orderid);
+        if (orderid != -1) {
+            Log.d("infoOrder", "insert order successful");
+            cartList = new DAOCart(this).getListCart(sessionManager.getUserID());
+            Log.d("infoOrder", "Số lượng cart là : " + cartList.size());
+            OrderDetail orderDetail = new OrderDetail();
+            for (Cart cart : cartList) {
+                Log.d("infoOrder", "Cart : " + cart);
+                orderDetail.setOrderID(orderid);
+                orderDetail.setProductID(cart.getProductID());
+                orderDetail.setQuantity(cart.getQuantity());
+                Log.d("infoOrder", "Order Detail add vào db : " + orderDetail);
+                daoOrderDetail.AddOrderDetail(orderDetail);
+            }
 //        for (Product product: productList) {
 //            orderDetail.setOrderID(order.getOrderID());
 //            orderDetail.setProductID(product.getProductID());
 //        }
-              Log.d("infoOrder","insert order detail successful" );
-              Intent intent = new Intent(this, ListUserOrderActivity.class);
-              startActivity(intent);
-       }else {
-           Log.d("infoOrder","insert order fail" );
-       }
+            Log.d("infoOrder", "insert order detail successful");
+            Intent intent = new Intent(this, ListUserOrderActivity.class);
+            startActivity(intent);
+        } else {
+            Log.d("infoOrder", "insert order fail");
+        }
 
 
     }
@@ -155,31 +164,31 @@ Button checkoutBtn;
     }
 
 
-    public void LoadRecyclerView(List<Cart> cartList, int id){
+    public void LoadRecyclerView(List<Cart> cartList, int id) {
         double total = 0;
         int pID = 0;
         int cartID = 0;
         List<Product> productList = new ArrayList<>();
         Product product = new DAOProduct(this).getProduct(id);
-        if(product != null) {
+        if (product != null) {
             for (Cart c : cartList) {
-                if(c.getProductID() == id){
+                if (c.getProductID() == id) {
                     pID = id;
                     c.setQuantity(c.getQuantity() + 1);
                     int n = new DAOCart(this).UpdateCart(c);
                 }
 
             }
-            if(pID != id){
+            if (pID != id) {
                 long n = new DAOCart(this).AddCart(new Cart(userID, product.getProductID(), 1));
-                cartList.add(new Cart(new DAOCart(this).getMaxCartID(),userID, product.getProductID(), 1));
+                cartList.add(new Cart(new DAOCart(this).getMaxCartID(), userID, product.getProductID(), 1));
             }
-            for(int i =0; i< cartList.size(); i++) {
+            for (int i = 0; i < cartList.size(); i++) {
                 Log.d("CartID:", String.valueOf(cartList.get(i).getCartID()));
             }
 
         }
-        if(cartList.size() != 0) {
+        if (cartList.size() != 0) {
             for (Cart c : cartList) {
                 Product p = new DAOProduct(this).getProduct(c.getProductID());
                 total += p.getPrice() * c.getQuantity();
@@ -189,14 +198,13 @@ Button checkoutBtn;
             adapter = new FoodAdapter(cartList, total, productList, this);
             rcv.setLayoutManager(new LinearLayoutManager(this));
             rcv.setAdapter(adapter);
-        }else{
+        } else {
 
-            ((ImageView)findViewById(R.id.imv_cartEmpty)).setVisibility(View.VISIBLE);
-            ((Button)findViewById(R.id.btn_checkout)).setVisibility(View.GONE);
+            ((ImageView) findViewById(R.id.imv_cartEmpty)).setVisibility(View.VISIBLE);
+            ((Button) findViewById(R.id.btn_checkout)).setVisibility(View.GONE);
             tv_total.setText("$0");
             rcv.setVisibility(View.GONE);
         }
-
 
 
     }
